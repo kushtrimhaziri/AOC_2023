fcd3953e5b9e3fcca1f33d135c900d6fdc90df85
Kushtrim Haziri <kushtrim.h@gjirafa.com>
2023-12-03 13:53:20 +0100

    Day 2 solution

--- days/day_02/main.go
+++ days/day_02/main.go
@@ -2,6 +2,8 @@ package day_02
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 // Run function of the daily challenge
@@ -16,10 +18,77 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	result := 0
+	for _, value := range input {
+		parts := strings.SplitN(value, ":", 2)
+		gameNumber := strings.SplitN(parts[0], " ", 2)[1]
+		rounds := strings.Split(parts[1], ";")
+
+		isValid := true
+		for _, value := range rounds {
+			var Numbers map[string]int = map[string]int{
+				"red":   12,
+				"green": 13,
+				"blue":  14,
+			}
+			sets := strings.Split(value, ",")
+			for _, value := range sets {
+				numberAndColor := strings.Fields(value)
+				number, err := strconv.Atoi(numberAndColor[0])
+				if err == nil {
+					Numbers[numberAndColor[1]] = Numbers[numberAndColor[1]] - number
+				}
+
+				if Numbers[numberAndColor[1]] < 0 {
+					isValid = false
+					break
+				}
+			}
+		}
+
+		if isValid {
+			gameNumberAsInt, error := strconv.Atoi(gameNumber)
+			if error == nil {
+				result += gameNumberAsInt
+			}
+		}
+	}
+	finalResult := strconv.Itoa(result)
+	return finalResult
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	finalSum := 0
+	for _, value := range input {
+		var Numbers map[string]int = map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
+		parts := strings.SplitN(value, ":", 2)
+		rounds := strings.Split(parts[1], ";")
+
+		for _, value := range rounds {
+			sets := strings.Split(value, ",")
+			for _, value := range sets {
+				numberAndColor := strings.Fields(value)
+				number, err := strconv.Atoi(numberAndColor[0])
+				if err == nil {
+					if number > Numbers[numberAndColor[1]] {
+						Numbers[numberAndColor[1]] = number
+					}
+				}
+			}
+		}
+
+		result := 1
+		for _, value := range Numbers {
+			result *= value
+		}
+		finalSum += result
+	}
+
+	finalResult := strconv.Itoa(finalSum)
+	return finalResult
 }
